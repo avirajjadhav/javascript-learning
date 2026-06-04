5ac8f721b039c11e27c39fe70c1c430f909f4f06
avirajjadhav <avirajjadhav0811@gmail.com>
2026-06-04 10:07:25 +0530

    variable declaration

---
+++ _01basics/01_variables.js
@@ -0,0 +1,16 @@
+// var x=10;
+// var x=20;
+// x=30;
+// console.log(x);
+
+//block code scope 
+if(true){
+    let age =30;
+    console.log(age);
+
+}
+
+const country="usa";
+console.log(country);
+
+const country="india"
