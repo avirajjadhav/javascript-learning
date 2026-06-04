0609be369e2e83ec22683799846d5f2ac3675aae
avirajjadhav <avirajjadhav0811@gmail.com>
2026-06-04 20:45:19 +0530

    datatype basics

---
+++ _01basics/02_datatypes.js
@@ -0,0 +1,44 @@
+//number datatype
+let a=10;
+// console.log(a);
+
+let n = 1.3;
+// console.log(n2);
+
+
+// console.log(typeof a,typeof n);
+
+
+
+
+//String
+
+let name="aviraj";
+let s="123" //this is also string 
+// console.log(name);
+// console.log(s);
+// console.log(typeof name, typeof s);
+
+// Boolean
+
+let b1=true;
+// 
+
+//null 
+
+let age=null;
+
+// console.log(name);
+// console.log(typeof age);// type of null is a object
+
+
+
+//undefined 
+
+let marks;
+console.log(marks);
+console.log(typeof marks);
+
+
+
+
