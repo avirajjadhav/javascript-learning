f076447fddd77614e1ecac0021f2851c1428fa1c
avirajjadhav <avirajjadhav0811@gmail.com>
2026-06-05 12:45:47 +0530

    Operators practice

---
+++ _01basics/03_operators.js
@@ -0,0 +1,58 @@
+//1 Arithmetic Operators
+const sum=5+3;
+const sub=5-1;
+const mul=5*2;
+const div=10/2;
+
+// console.log(sum,sub,mul,div);
+
+//Assignment Operators
+
+let num=10;
+
+num +=5;
+
+num *=2;
+
+num -=10;
+
+// console.log(num);
+
+// comparison operators .
+
+let x=5;
+let y=6;
+// console.log(x<y);
+// console.log(x==y);
+// console.log(x!==y);
+// console.log(x>y);
+// console.log(x<=y);
+
+//4 Logical Operators
+
+let num1 = 6;
+let num2 = 3;
+let z = (num1 < 10 && num2 > 1);
+// console.log(z);
+
+let a=5;
+let b=3
+let r=(a<=10 || b>10)
+// console.log(r);
+
+// console.log(!(5 > 3));
+
+//5 conditional Operators
+
+const age=18;
+const status = age>=18 ?"adult":"minor";
+// console.log(status);
+
+const marks=50;
+const result = marks>=32 ? "pass":"fail"
+console.log(result);
+
+
+
+
+
