897ea1d5aadc65d5427caeb3f74e4f2b00c7f4ba
avirajjadhav <avirajjadhav0811@gmail.com>
2026-06-08 20:49:44 +0530

    Even or Odd Number  -program

---
+++ 02_Control_flow/if-else/even-odd.js
@@ -0,0 +1,23 @@
+let num=3;
+
+if(num % 2== 0)
+{
+    console.log("number is even");
+    
+}else{
+    console.log("number is odd");
+    
+}
+
+//method 2 using switch case
+let num = 14;
+
+switch (num % 2) {
+    case 0:
+        console.log("Even Number");
+        break;
+    // case 1:
+    case 1:
+        console.log("Odd Number");
+        break;
+}
