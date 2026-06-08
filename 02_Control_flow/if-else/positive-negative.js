41eed17f2a0d468b63100174bf422c4271f119be
avirajjadhav <avirajjadhav0811@gmail.com>
2026-06-08 18:53:11 +0530

    positive or negative number program

---
+++ 02_Control_flow/if-else/positive-negative.js
@@ -0,0 +1,24 @@
+let num=-9;
+
+// if(num>0){
+//     console.log("positive number");
+    
+// }else if(num<0)
+// {
+//     console.log("negative number");
+    
+// }else{
+//     console.log("number is zero");
+    
+// }
+
+//method 2Using Ternary Operator
+
+let num2=0;
+
+let result= num2>0 ? "positive"  :
+            num2<0 ? "negative" :"zero";
+          
+
+console.log(result);
+
