4513f921fbe5b957a77fe8a4fdb9ee5da218efab
avirajjadhav <avirajjadhav0811@gmail.com>
2026-06-08 21:08:12 +0530

    Sum of First N Natural Numbers program using loop

---
+++ 03_loops/sum-of-first_n.js
@@ -0,0 +1,18 @@
+// let n=5;
+// let sum=0;
+
+// for(let i=1;i<=5;i++){
+//     sum +=i;
+// }
+// console.log("sum =",sum);
+
+//method 2 
+let n=5;
+let sum=0;
+let i=1;
+
+while(i<=n){
+    sum +=i;
+    i++;
+}
+console.log(sum);
