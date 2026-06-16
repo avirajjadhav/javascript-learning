6143710f6cffb7c777596b7ac83c62d0c8342ff4
avirajjadhav <avirajjadhav0811@gmail.com>
2026-06-16 09:53:27 +0530

    greatest-of-two-numbers program

---
+++ 02_Control_flow/greatest-of-two-numbers.js
@@ -0,0 +1,17 @@
+let num1=20;
+let num2=45;
+
+if(num1>num1)
+{
+    console.log(num1 + " is greater");
+    
+}else if(num2>num1)
+{
+    console.log(num2 + "is greater ");
+    
+}
+else
+{
+    console.log("both numbers are equal");
+    
+}
