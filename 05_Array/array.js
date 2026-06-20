c5958831b0109113bacf52c30a77f6eec69bbe6a
avirajjadhav <avirajjadhav0811@gmail.com>
2026-06-20 13:10:30 +0530

    array examples

---
+++ 05_Array/array.js
@@ -0,0 +1,29 @@
+//creating and initializing with values
+
+let a=["html","css","javascript"]
+
+// console.log(a[0]);
+// console.log(a[1]);
+
+// console.log("array Elements :"+ a);
+
+
+//2 .accessing the first element of an array.
+
+let c=["BMW","Volvo","Audi","Fortuner"];
+
+let fst=c[0];
+
+// console.log("first Item:" + fst)
+// console.log(c[0]);
+
+
+//accessing the last element of an array
+
+let b=["Html","css","js"]
+
+let lst=b[b.length-1];
+console.log("last item:",lst);
+
+console.log(b[2]);
+
