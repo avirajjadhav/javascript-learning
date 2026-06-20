09b9175bb1af33d209f4a8c2d5bf4d23bd834a9b
avirajjadhav <avirajjadhav0811@gmail.com>
2026-06-20 11:31:37 +0530

    conversionOperation

---
+++ 04_conversionOperation/conversionOperation.js
@@ -0,0 +1,31 @@
+let score="aviraj"
+
+console.log(typeof score);
+
+//console.log(typeof(score));
+
+let valueInNumber=Number(score)
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+
+let isLoggedIn="avi";
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+
+//console.log(typeof isLoggedIn);
+
+//console.log(booleanIsLoggedIn);
+
+let someNumber=10;
+
+let stringNumber=String(someNumber)
+
+// console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//conversion done 
+//   string => Number
+//   string => Boolean
+//   number =>  string
