7b2d2cbccf4babbf42f05beb7955e39f443e4dc9
avirajjadhav <avirajjadhav0811@gmail.com>
2026-06-27 12:28:39 +0530

    greatest_of_three numbers

---
+++ 02_Control_flow/if-else/greatest_of_three_num.js.js
@@ -0,0 +1,11 @@
+let num1=300;
+let num2=40;
+let num3= 5;
+
+if(num1>=num2&& num1>=num3){
+    console.log(num1+": num 1 is a greater number");
+}else if(num2>=num1 && num2>=num3){
+    console.log(num2+": num 2 is a greater number")
+}else{
+    console.log(num3 +": num 3is a greater number")
+}
