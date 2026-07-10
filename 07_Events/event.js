bd93752eea3800a17efb54219a4b88000e519784
avirajjadhav <avirajjadhav0811@gmail.com>
2026-07-10 19:17:21 +0530

    event js file

--- 07_Events/event.js
+++ 07_Events/event.js
@@ -1,54 +1,54 @@
-// function myFun() {
-//   document.getElementById("gfg").innerHTML = Date();
-//   document.getElementById("gfg").style.background = "cyan";
-// }
-// function action(){
-//  alert("mouse move over");
-// }
-// function msoutaction(){
-//   alert("mouse move out");
-// }
+function myFun() {
+  document.getElementById("gfg").innerHTML = Date();
+  document.getElementById("gfg").style.background = "cyan";
+}
+function action(){
+ alert("mouse move over");
+}
+function msoutaction(){
+  alert("mouse move out");
+}
 
-// document.getElementById("inputfield").addEventListener("keypress",keydownAction);
-// function keydownAction() {
-//   document.getElementById("inputfield").style.backgroundColor = "orange";
-// }
-// function myFunction() {
-//   alert("You pressed a key inside the input field");
-// }
-//  function Displaycar(){
-//     let x=document.getElementById("cars").value;
-//     document.getElementById('displayvalue').innerHTML=" selected Car : " + x;
+document.getElementById("inputfield").addEventListener("keypress",keydownAction);
+function keydownAction() {
+  document.getElementById("inputfield").style.backgroundColor = "orange";
+}
+function myFunction() {
+  alert("You pressed a key inside the input field");
+}
+ function Displaycar(){
+    let x=document.getElementById("cars").value;
+    document.getElementById('displayvalue').innerHTML=" selected Car : " + x;
 
-//   }
-//   function myFunction() {
-//   let text = document.getElementById("myInput").value;
-//   document.getElementById("demo").innerHTML = "You write: " + text;
-// }
+  }
+  function myFunction() {
+  let text = document.getElementById("myInput").value;
+  document.getElementById("demo").innerHTML = "You write: " + text;
+}
 
-// window.addEventListener("load", function () {
-//     alert("Page Loaded Successfully!");
-// });
+window.addEventListener("load", function () {
+    alert("Page Loaded Successfully!");
+});
 
-// const form = document.getElementById("myform");
+const form = document.getElementById("myform");
 
-// form.addEventListener("submit", function(event) {
+form.addEventListener("submit", function(event) {
 
-//     event.preventDefault();
+    event.preventDefault();
 
-//     alert("Form Submitted Successfully!");
+    alert("Form Submitted Successfully!");
 
-// });
+});
 
-// const input = document.getElementById("username");
+const input = document.getElementById("username");
 
-// input.addEventListener("focus", Myfocus); 
-// function Myfocus() {
-//   document.getElementById("username").style.backgroundColor = "yellow";
-// }
+input.addEventListener("focus", Myfocus); 
+function Myfocus() {
+  document.getElementById("username").style.backgroundColor = "yellow";
+}
 
-// document.getElementById("blurevent").addEventListener("blur", myFunction1);
+document.getElementById("blurevent").addEventListener("blur", myFunction1);
 
-// function myFunction1() {
-//   alert("Input field lost focus.");
-// }
+function myFunction1() {
+  alert("Input field lost focus.");
+}
