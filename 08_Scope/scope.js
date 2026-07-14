b8be3f3f53fc5239981dcacfa50cf1893f3e33c5
avirajjadhav <avirajjadhav0811@gmail.com>
2026-07-14 22:14:53 +0530

    js scope add

--- 08_Scope/scope.js
+++ 08_Scope/scope.js
@@ -39,7 +39,7 @@ function calculate() {
 
 calculate();
 
-console.log(total);
+// console.log(total);
 
 
 //Block Scope
@@ -48,7 +48,7 @@ if (true) {
   console.log(age);
 }
 
-console.log(age); 
+// console.log(age); 
 
 "use strict";
 
