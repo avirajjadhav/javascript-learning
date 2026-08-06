082b50116eac412185df69d6a4e638f2fdd10d4a
avirajjadhav <avirajjadhav0811@gmail.com>
2026-08-06 21:45:39 +0530

    understanding web storage

---
+++ 10_Web_Storage/script.js
@@ -0,0 +1,8 @@
+sessionStorage.setItem('name','aviraj')
+sessionStorage.setItem('course','Bca');
+//display result on page.
+// document.getElementById("result").innerHTML='<h3>name:'+sessionStorage.getItem("name")+'</h3> <h3>Course:'+sessionStorage.getItem('course')+'</h3>';
+
+
+localStorage.setItem('name','aviraj')
+localStorage.setItem('course','Bca');
